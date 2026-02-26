a85ad829183adec69cb4b424a79b32d20ed2efc3
Jabralin M <jabralinmckinney@gmail.com>
2026-02-25 18:17:42 -0600

    Imported assets and setting up input movment fort the rocket

--- Assets/Scirpts/Movement.cs
+++ Assets/Scirpts/Movement.cs
@@ -1,17 +1,53 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStrength = 100f;
+    [SerializeField] float rotationStrength = 100f;
+
+    Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        thrust.Enable();
+        rotation.Enable();
+    }
+
+    void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            transform.Rotate(Vector3.forward * rotationStrength * Time.fixedDeltaTime);
+        }
+        else if(rotationInput > 0)
+        {
+            transform.Rotate(Vector3.forward * -rotationStrength * Time.fixedDeltaTime);
+        }
     }
 }
+    
